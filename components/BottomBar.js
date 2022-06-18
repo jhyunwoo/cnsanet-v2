670196391f647bd8c3cdfa0c8bac96dec5a9fa0c
jhyunwoo <jhyunwoo0228@gmail.com>
2022-06-19 02:19:49 +0900

    before dark mode

--- components/BottomBar.js
+++ components/BottomBar.js
@@ -31,7 +31,7 @@ export default function BottomBar() {
 
   return (
     <div>
-      <div className="flex flex-row justify-between bg-white/50 backdrop-blur-sm m-2 p-2 rounded-xl text-[#2e2a38] fixed bottom-0 right-0 left-0 shadow-xl">
+      <div className="flex flex-row justify-between bg-white/50 backdrop-blur-sm m-2 p-2 rounded-xl text-[#2e2a38] fixed bottom-0 right-0 left-0 shadow-xl dark:bg-black/50 dark:text-white">
         <Link href={"/classes"}>
           <div>
             <LibraryIcon className="h-8 w-8" />
@@ -57,7 +57,7 @@ export default function BottomBar() {
         </Link>
       </div>
       <div
-        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white/50 p-4 rounded-2xl flex flex-col backdrop-blur-xl bg-blend-color-burn ${
+        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white/50 dark:bg-black/50 dark:text-white p-4 rounded-2xl flex flex-col backdrop-blur-xl bg-blend-color-burn ${
           menuUp ? "visible" : "invisible"
         }`}
       >
@@ -70,7 +70,7 @@ export default function BottomBar() {
         <div>
           {menuOptions.map((menu, menuIdx) => (
             <Link key={menuIdx} href={`${menu.link}`}>
-              <div className="m-2 text-lg bg-slate-50/70 rounded-lg p-2 px-4 flex flex-row justify-between backdrop-blur-xl hover:bg-sky-200 transition">
+              <div className="m-2 text-lg bg-slate-50/70 dark:bg-slate-800/70 rounded-lg p-2 px-4 flex flex-row justify-between backdrop-blur-xl hover:bg-sky-200 dark:hover:bg-sky-700 transition">
                 <div>{menu.op}</div>
                 <ChevronDoubleRightIcon className="w-4 h-4 my-auto ml-2" />
               </div>
