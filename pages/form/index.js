0acc5dce7f77215016f0dab0a7a6a3494fc6ec50
jhyunwoo <jhyunwoo0228@gmail.com>
2022-06-19 00:04:53 +0900

    remove selector icon from form page

--- pages/form/index.js
+++ pages/form/index.js
@@ -60,12 +60,7 @@ export default function Form() {
               <div className=" mt-1">
                 <Listbox.Button className=" w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                   <span className="block truncate">{selected.name}</span>
-                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    <SelectorIcon
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"></span>
                 </Listbox.Button>
                 <Transition
                   as={Fragment}
