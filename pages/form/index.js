ee4a71c3eff48e6c2e0a5b03459b9cec2bf78932
jhyunwoo <jhyunwoo0228@gmail.com>
2022-06-19 00:03:01 +0900

    before firebase settings

--- pages/form/index.js
+++ pages/form/index.js
@@ -95,14 +95,6 @@ export default function Form() {
                             >
                               {person.name}
                             </span>
-                            {selected ? (
-                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                                <CheckIcon
-                                  className="h-5 w-5"
-                                  aria-hidden="true"
-                                />
-                              </span>
-                            ) : null}
                           </>
                         )}
                       </Listbox.Option>
