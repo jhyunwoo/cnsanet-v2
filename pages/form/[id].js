80cd1ba5e0bc0999a233429b6ca8c0344805cbd8
jhyunwoo <jhyunwoo0228@gmail.com>
2022-06-19 00:21:58 +0900

    add profile bar and bottom bar in form detail page

--- pages/form/[id].js
+++ pages/form/[id].js
@@ -1,3 +1,12 @@
+import BottomBar from "../components/BottomBar";
+import ProfileBar from "../components/ProfileBar";
+
 export default function Forms() {
-  return <div className="text-center text-4xl my-96">🚧개발 예정🚧</div>;
+  return (
+    <div>
+      <ProfileBar />
+      <BottomBar />
+      <div className="text-center text-4xl my-96">🚧개발 예정🚧</div>
+    </div>
+  );
 }
